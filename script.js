721754cd7c3297eec05e989e80bb452c6501931d
lord20091 <abdulazizbozorov009@gmail.com>
2024-05-03 17:28:18 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let haftaKuni = prompt(`biron hafta kunni sonini kiriting`)
+
+function hafta(x) {
+
+    switch (x) {
+        case `1`:
+            alert(`Dushanba`)
+            break;
+        case `2`:
+            alert(`Seshanba`)
+            break;
+        case `3`:
+            alert(`Chorshanba`)
+            break;
+        case `4`:
+            alert(`Payshanba`)
+            break;
+        case `5`:
+            alert(`Juma`)
+            break;
+        case `6`:
+            alert(`Shanba`)
+            break;
+        case `7`:
+            alert(`Yakshanba`)
+            break;
+        default:
+            alert(`bunaqa sonli hafta kuni yo`)
+    }
+}
+
+hafta(haftaKuni)
+
